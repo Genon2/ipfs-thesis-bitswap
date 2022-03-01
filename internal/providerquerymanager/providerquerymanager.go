f670d9617906c52106a70b2d90f5ae14b3470683
Whyros <sebastienpierre1995@gmail.com>
2022-03-01 13:41:08 +0100

    essai DHT doesn't work

--- internal/providerquerymanager/providerquerymanager.go
+++ internal/providerquerymanager/providerquerymanager.go
@@ -252,6 +252,7 @@ func (pqm *ProviderQueryManager) findProviderWorker() {
 
 						return
 					} else {
+						fmt.Printf("[%s] TRY !!!!\n", pqm.ratio)
 						pqm.ratio.CheckDHT(k.String())
 						fmt.Printf("[%s] DHT TRUED !!!!\n", pqm.ratio.ToString())
 						fmt.Printf("[%s] found by provider - %s\n", k.String(), p)
